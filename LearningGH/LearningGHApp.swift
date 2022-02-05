e21166cebd1e9d6b8961a691fcd80382097e23c4
lukasz.filiciak <lukasz.filiciak@farmaprom.pl>
2022-02-05 08:32:37 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Lukasz Filiciak on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
